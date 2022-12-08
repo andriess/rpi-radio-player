3a869c23239aa4327d476ab82924df573e06207d
Andries <Andries.Schaap@rakuten.com>
2022-12-08 12:22:31 +0000

    mocking?

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -5,14 +5,25 @@ from pigpio_encoder.rotary import Rotary
 from mpd import MPDClient
 
 import unittest
-
+import mock
 
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
+    @mock.patch('rpi_radio_player.models.StationModel')
+    @mock.patch('rpi_radio_player.views.StationListView')
+    @mock.patch('mpd.MPDClient')
+    @mock.patch('pigpio_encoder.rotary.Rotary')
+    def test_passes_when_controller_created_with_correct_types(self, mock_model, mock_view, mock_input, mock_player):
+        rpi_radio_player.controller.RadioController(mock_model, mock_view, mock_input, mock_player)
+
     def test_fails_when_input_is_not_Rotary_type(self):
         with self.assertRaises(TypeError):
-            rpi_radio_player.controller.RadioController(None, None, MPDClient, MPDClient())
+            rpi_radio_player.controller.RadioController(None, None, MPDClient, MPDClient)
+
+    def test_fails_when_player_is_not_MPDClient_type(self):
+        with self.assertRaises(TypeError):
+            rpi_radio_player.controller.RadioController(None, None, Rotary(0,0,0), MPDClient)
 
 
 if __name__ == '__main__':
