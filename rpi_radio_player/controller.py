f60f4fcb8b36a3deb9913aefdac81a199c576780
Andries <Andries.Schaap@rakuten.com>
2022-12-08 12:39:33 +0000

    I'm very close to giving up on type checking.

--- rpi_radio_player/controller.py
+++ rpi_radio_player/controller.py
@@ -1,27 +1,16 @@
 from mpd import MPDClient
 from pigpio_encoder.rotary import Rotary
+from rpi_radio_player.models import StationModel
+from rpi_radio_player.views import StationListView
 
 class RadioController():
-    def __init__(self, model, view, input: Rotary, player: MPDClient):
-        if not isinstance(input, Rotary):
-            raise TypeError("Input should be of type: Rotary")
-        if not isinstance(player, MPDClient):
-            raise TypeError("player should be of type: MPDClient")
-
+    def __init__(self, model, view):
+        if not isinstance(model, StationModel):
+            raise TypeError("Input should be of type: StationModel")
+        if not isinstance(view, StationListView):
+            raise TypeError("player should be of type: StationListView")
         self._model = model
         self._view = view
-        self._input = input
-        self._player = player
-
-        # Why am I doing the init here. Makes more sense to do this as an app start action.
-        #self._initPlayer()
-
-        input.setup_rotary(
-            up_callback=self._up_callback,
-            down_callback=self._down_callback,
-            )
-        input.setup_switch(sw_short_callback=self._sw_short)
-        input.watch()
 
     def _sw_short(self):
         currentStationPos = self._model.get_current_station_position
@@ -39,3 +28,32 @@ class RadioController():
         stations = self._model.get_all_station_urls()
         for station in stations:
             self._player.add(station.url)
+
+class InputController():
+    def __init__(self, input: Rotary, player: MPDClient):
+        if not isinstance(input, Rotary):
+            raise TypeError("Input should be of type: Rotary")
+        if not isinstance(player, MPDClient):
+            raise TypeError("player should be of type: MPDClient")
+
+        self._input = input
+        self._player = player
+
+        # Why am I doing the init here. Makes more sense to do this as an app start action.
+        #self._initPlayer()
+
+        input.setup_rotary(
+            up_callback=self._up_callback,
+            down_callback=self._down_callback,
+            )
+        input.setup_switch(sw_short_callback=self._sw_short)
+        input.watch()
+
+    def _up_callback(self, counter):
+        pass
+
+    def _down_callback(self, counter):
+        pass
+
+    def _sw_short(self):
+        pass
--- tests/test_controller.py
+++ tests/test_controller.py
@@ -12,10 +12,8 @@ class BasicTestSuite(unittest.TestCase):
 
     @mock.patch('__main__.StationModel')
     @mock.patch('__main__.StationListView')
-    @mock.patch('__main__.MPDClient')
-    @mock.patch('__main__.Rotary')
     def test_passes_when_controller_created_with_correct_types(self, mock_model, mock_view, mock_input, mock_player):
-        rpi_radio_player.controller.RadioController(mock_model, mock_view, mock_input, mock_player)
+        rpi_radio_player.controller.RadioController(mock_model, mock_view)
 
     def test_fails_when_input_is_not_Rotary_type(self):
         with self.assertRaises(TypeError):
@@ -23,7 +21,7 @@ class BasicTestSuite(unittest.TestCase):
 
     def test_fails_when_player_is_not_MPDClient_type(self):
         with self.assertRaises(TypeError):
-            rpi_radio_player.controller.RadioController(None, None, Rotary(0,0,0), MPDClient)
+            rpi_radio_player.controller.RadioController(None, None, Rotary(1,2,3), MPDClient)
 
 
 if __name__ == '__main__':
