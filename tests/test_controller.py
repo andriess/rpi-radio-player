2002ace17129a5f5007d30a8816f014cbed0adad
Andries <Andries.Schaap@rakuten.com>
2022-12-13 16:10:25 +0000

    Did some linting, moved some stuff around.

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from rpi_radio_player.models import StationModel
 from .context import rpi_radio_player
 from pigpio_encoder.rotary import Rotary
 from mpd import MPDClient
@@ -10,18 +11,16 @@ from unittest import mock
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
-    @mock.patch('rpi_radio_player.models.StationModel')
-    @mock.patch('rpi_radio_player.views.StationListView')
-    def test_passes_when_controller_created_with_correct_types(self, mock_model, mock_view):
-        rpi_radio_player.controller.RadioController(mock_model, mock_view)
+    def test_passes_when_controller_created_with_correct_types(self):
+        rpi_radio_player.controllers.RadioController()
 
     def test_fails_when_input_is_not_Rotary_type(self):
         with self.assertRaises(TypeError):
-            rpi_radio_player.controller.RadioController(None, None, MPDClient, MPDClient)
+            rpi_radio_player.controllers.RadioController(None, None, MPDClient, MPDClient)
 
     def test_fails_when_player_is_not_MPDClient_type(self):
         with self.assertRaises(TypeError):
-            rpi_radio_player.controller.RadioController(None, None, Rotary(1,2,3), MPDClient)
+            rpi_radio_player.controllers.RadioController(None, None, Rotary(1,2,3), MPDClient)
 
 
 if __name__ == '__main__':
